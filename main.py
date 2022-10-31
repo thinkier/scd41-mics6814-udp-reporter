392f73c2ff5b56828336bc1cf3e1e54cbb4481ea
thinkier <26728734+thinkier@users.noreply.github.com>
2022-11-01 00:00:30 +1100

    initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import gc
+import time
+import network
+import socket
+import ujson as json
+
+from secrets import WIFI_SSID, WIFI_PASS
+from sensor import CO2Sensor, COSensor
+
+ADDR = ("192.168.1.69", 5555)
+
+def wifi_connect():
+    rp2.country('AU')
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(WIFI_SSID, WIFI_PASS)
+
+    # WiFi Connection code from https://datasheets.raspberrypi.com/picow/connecting-to-the-internet-with-pico-w.pdf
+    max_wait = 10
+    for _ in range(max_wait):
+        status = wlan.status()
+        if status < 0 or status >= 3:
+            break
+        time.sleep(1)
+
+    if wlan.status() != 3:
+        print('wifi connection failed')
+        wlan.active(False)
+
+    ip = wlan.ifconfig()[0]
+    print('WiFi connected; ip = ' + ip)
+    return wlan
+
+
+wifi_connect()
+s = CO2Sensor()
+s2 = COSensor()
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+while True:
+    payload = json.dumps({'temperature': s.temp, 'humidity': s.hum, 'co2_ppm': s.co2, 'co_ppm': s2.co})
+    sock.sendto(payload, ADDR)
+    gc.collect()
+    time.sleep(1.0)
+
---
+++ sensor.py
@@ -0,0 +1,51 @@
+import math
+import time
+
+import pimoroni_i2c
+import breakout_scd41
+from breakout_mics6814 import BreakoutMICS6814
+
+from machine import Timer
+
+
+PINS_BREAKOUT_GARDEN = {"sda": 4, "scl": 5}
+i2c = pimoroni_i2c.PimoroniI2C(**PINS_BREAKOUT_GARDEN)
+
+class CO2Sensor:
+    co2 = 0
+    temp = 0
+    hum = 0
+    
+    def __init__(self):
+        global i2c
+        breakout_scd41.init(i2c)
+        breakout_scd41.start()
+
+        self.sensor_timer = Timer(period=1000, mode=Timer.PERIODIC, callback=self.measure_task)
+
+    def measure(self):
+        self.co2, self.temp, self.hum = breakout_scd41.measure()
+
+    def measure_task(self, timer: Timer):
+        if not breakout_scd41.ready():
+            return # Avoid backpressure issues with time.sleep_ms()
+        self.measure()
+    
+
+class COSensor:
+    co = 0
+    
+    def __init__(self):
+        global i2c
+        self.gas = BreakoutMICS6814(i2c)
+        self.sensor_timer = Timer(period=1000, mode=Timer.PERIODIC, callback=self.measure_task)
+        
+    def measure_task(self, timer: Timer):
+        self.measure()
+    
+    def measure(self):
+        # https://github.com/Seeed-Studio/Mutichannel_Gas_Sensor/blob/master/MutichannelGasSensor.cpp#L333
+        red = self.gas.read_reducing()
+        self.co = math.pow(red, -1.179) * 4.385 * 1e3
+
+
